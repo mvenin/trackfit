8ffd1a77c18431894f0c8ca24526fe87c1a3105d
Marian Venin <marian@veninsoft.com>
2017-01-20 23:43:06 +0100

    add secu

--- src/main/java/ro/mve/trackfit/StartApplication.java
+++ src/main/java/ro/mve/trackfit/StartApplication.java
@@ -7,13 +7,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableOAuth2Client
 public class StartApplication extends SpringBootServletInitializer {
-
+	
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(StartApplication.class);
