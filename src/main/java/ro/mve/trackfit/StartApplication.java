e7a4390785ca08ab25dc521fa514a1210867abbc
Marian Venin <marian@veninsoft.com>
2017-01-20 22:56:59 +0100

    add web support

--- src/main/java/ro/mve/trackfit/StartApplication.java
+++ src/main/java/ro/mve/trackfit/StartApplication.java
@@ -5,27 +5,32 @@ import java.util.Date;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class StartApplication 
-{
+public class StartApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(StartApplication.class);
+	}
 
-    public static void main(String[] args) {
-        SpringApplication.run(StartApplication.class, args);
-    }
-    
-    @RequestMapping("/user")
-    public Principal user(Principal principal) {
-        return principal;
-    }
-    @RequestMapping("/echo")
-    public String user() {
-    	return "echo " + new Date().toString();
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(StartApplication.class, args);
+	}
 
-   
+	@RequestMapping("/user")
+	public Principal user(Principal principal) {
+		return principal;
+	}
+
+	@RequestMapping("/echo")
+	public String user() {
+		return "echo " + new Date().toString();
+	}
+
 }
